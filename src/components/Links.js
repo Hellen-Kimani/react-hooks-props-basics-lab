3d19b88195f82a134b552ea9b3fc541178a1d732
hellen kimani <astridlee80@gmail.com>
2024-05-12 19:12:01 +1000

    First commit

---
+++ src/components/Links.js
@@ -0,0 +1,9 @@
+export default function Links (links){
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={links.github}>{links.github}</a>
+            <a href={links.linkedin}>{links.linkedin}</a>
+        </div>
+    )
+}
